3b6d66e39d553e7a032260e5768675ceacbe858b
Kevin Upton <k_upton@live.com>
2017-01-11 09:58:14 +1000

    completed insertion sort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -3,9 +3,27 @@
 
 #include "stdafx.h"
 
+int *insertionSort(int *unsortedArray, int length);
 
 int main()
 {
     return 0;
 }
 
+int *insertionSort(int *unsortedArray, int length) {
+	int *sortedArray = new int[length];
+
+	for (int i = 0; i < length; i++) {
+		int position = i;
+		int positionValue = unsortedArray[i];
+
+		while (position > 0 && sortedArray[position - 1] > positionValue) {
+			sortedArray[position] = sortedArray[position - 1];
+			position--;
+		}
+
+		sortedArray[position] = positionValue;
+	}
+
+	return sortedArray;
+}
